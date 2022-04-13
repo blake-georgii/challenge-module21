7e7a987e38316bb524be52f96bfb981ce7ee81a3
Blake Georgii <blakegeorgii@gmail.com>
2022-04-13 16:43:39 -0700

    Updates

---
+++ precache-manifest.1c16e7f61d97323953560dee2820ce4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed56dad6f6d07d1a719e7df7b00d2061",
+    "url": "/challenge-module21/index.html"
+  },
+  {
+    "revision": "d6d58ed741fff5699665",
+    "url": "/challenge-module21/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "cce21dac1f6cee563b40",
+    "url": "/challenge-module21/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "d6d58ed741fff5699665",
+    "url": "/challenge-module21/static/js/2.ac7c39ba.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/challenge-module21/static/js/2.ac7c39ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cce21dac1f6cee563b40",
+    "url": "/challenge-module21/static/js/main.5408f4c0.chunk.js"
+  },
+  {
+    "revision": "934869248387c5446111",
+    "url": "/challenge-module21/static/js/runtime-main.d617f540.js"
+  }
+]);
